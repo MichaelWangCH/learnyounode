97f436a0a5b54bbfe784b5a26c5cfc3c0549ce7e
Wang, Michael <ch_w@live.com>
2016-01-29 20:34:54 +0800

    solve exercise of HTTP JSON API SERVER

---
+++ learnyounode/HttpJSONAPIServer.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(request, response) {
+  if (request.method == 'GET') {
+    var urlObj = url.parse(request.url, true);
+    var requestUrl = urlObj.pathname;
+    var queryString = urlObj.query.iso;
+    var date = new Date(queryString);
+
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+
+    if (requestUrl == '/api/parsetime') {
+      var jsonObj = {hour: date.getHours(), minute: date.getMinutes(), second: date.getSeconds()};
+
+      response.end(JSON.stringify(jsonObj));
+    } else if (requestUrl == '/api/unixtime') {
+      var jsonObj = {unixtime: date.getTime()};
+
+      response.end(JSON.stringify(jsonObj));
+    } else {
+      res.writeHead(404)
+      res.end();
+    }
+  }
+});
+
+server.listen(Number(process.argv[2]));

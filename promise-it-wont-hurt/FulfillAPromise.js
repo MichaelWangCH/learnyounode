6fd13655a331a42fd5eb823d85e0d85326d86e99
Wang, Michael <ch_w@live.com>
2016-02-02 19:54:16 +0800

    solve exercise of Fulfill a promise of Promise It Won’t Hurt

---
+++ promise-it-wont-hurt/FulfillAPromise.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var promise = new Promise(function(fulfill, reject) {
+  setTimeout(() => fulfill('FULFILLED!'), 300);
+});
+
+promise.then(function(result) {
+  console.log(result);
+}, function(err) {
+  console.log(err);
+});
